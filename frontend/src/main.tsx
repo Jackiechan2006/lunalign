2ef2e373cb0449f84a2469798b9b4f13cb239ba7
P. Hanushree <hanushree20@gmail.com>
2026-09-05 09:17:06 +0530

    style(ui): load landing journey presentation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./workspace-polish.css";
+import "./landing-journey.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
